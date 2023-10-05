923e352634ab4a2e4c64febf16c44c0d4d6179a3
Ugotapi <rudischmitz@gmail.com>
2023-10-04 20:13:05 -0500

    Update 0-pagepi.sh

--- 0-pagepi.sh
+++ 0-pagepi.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/1-tablepi.sh
-wget https://raw.githubusercontent.com/rudischmitz71/blahblah/main/4-finalize-tablepi.sh
-cp /home/$USER/4-finalize-tablepi.sh /home/$USER/Desktop/4-finalize-tablepi.sh
+cd ~
+wget https://raw.githubusercontent.com/ugotapi/pagepi/main/1-pagepi.sh
 sudo chmod +x /home/$USER/*.sh
-/home/$USER/1-tablepi.sh
+/home/$USER/1-pagepi.sh
