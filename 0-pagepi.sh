e6889873d164a6994751ca1dc732f7234b90c418
Ugotapi <rudischmitz@gmail.com>
2023-10-10 08:01:00 -0500

    Update 0-pagepi.sh

--- 0-pagepi.sh
+++ 0-pagepi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ~
+sudo apt update 
+sudo apt upgrade -y
 wget https://raw.githubusercontent.com/ugotapi/pagepi/main/1-pagepi.sh
 sudo chmod +x /home/$USER/*.sh
 /home/$USER/1-pagepi.sh
