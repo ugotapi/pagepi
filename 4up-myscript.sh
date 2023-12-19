b23a96ba12b0bca132fb66a71500f372df25a964
Ugotapi <rudischmitz@gmail.com>
2023-12-19 12:38:10 -0600

    Update 4up-myscript.sh

--- 4up-myscript.sh
+++ 4up-myscript.sh
@@ -12,15 +12,15 @@ chromium-browser --new-window --window-position=0,0 --window-size=1920,1080 --in
 
 chromium-browser --new-window --window-position=1920,0 --window-size=1920,1080 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
---app=https://cnbc.com &
+--app=https://www.cnbc.com/us-markets/ &
 
 chromium-browser --new-window --window-position=0,1081 --window-size=1920,1280 --incognito --user-data-dir=/home/$USER/.config/chromium3 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
---app=https://www.wsj.com &
+--app=https://www.wsj.com/market-data?mod=finance_mdw_mdc &
 
 chromium-browser --new-window --window-position=1921,1081 --window-size=1920,1080 --incognito --user-data-dir=/home/$USER/.config/chromium4 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
---app=https://www.wsj.com &
+--app=https://www.barchart.com/stocks/market-performance &
 
 EOLP
 
