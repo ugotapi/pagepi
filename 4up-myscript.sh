3b1dfefc1cc2be0582a0501d1bd4df41dc63ae76
Ugotapi <rudischmitz@gmail.com>
2023-12-19 12:34:18 -0600

    Update and rename portrait-myscript.sh to 4up-myscript.sh

---
+++ 4up-myscript.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# 3up internet screens portait
+
+#overwrite existing myscript.sh
+
+cat > /home/$USER/myscript.sh << EOLP
+#!/bin/sh
+# what this script does: start chromium
+
+chromium-browser --new-window --window-position=0,0 --window-size=1920,1080 --incognito --user-data-dir=/home/$USER/.config/chromium1 \
+--enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
+--app=https://www.google.com/finance &
+
+chromium-browser --new-window --window-position=1920,0 --window-size=1920,1080 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
+--enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
+--app=https://cnbc.com &
+
+chromium-browser --new-window --window-position=0,1081 --window-size=1920,1280 --incognito --user-data-dir=/home/$USER/.config/chromium3 \
+--enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
+--app=https://www.wsj.com &
+
+chromium-browser --new-window --window-position=1921,1081 --window-size=1920,1080 --incognito --user-data-dir=/home/$USER/.config/chromium4 \
+--enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
+--app=https://www.wsj.com &
+
+EOLP
+
+
