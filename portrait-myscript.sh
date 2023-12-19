929a56abd9af2f9ddc4f6585a5c8bb02fb8b7c83
Ugotapi <rudischmitz@gmail.com>
2023-12-18 20:23:16 -0600

    Update portrait-myscript.sh

--- portrait-myscript.sh
+++ portrait-myscript.sh
@@ -3,7 +3,12 @@
 
 DISPLAY=:0 xrandr --output HDMI-1 --rotate right
 
+#overwrite existing myscript.sh
 
+cat > /home/$USER/myscript.sh << EOL
+
+#!/bin/sh
+# what this script does: start chromium
 
 chromium-browser --new-window --window-position=0,0 --window-size=2160,1280 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
@@ -17,6 +22,7 @@ chromium-browser --new-window --window-position=0,0 --window-size=2160,1280--inc
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://www.wsj.com &
 
+EOL  
 
 sudo reboot
 
