830ead82aba0b84a9da1c0cd9f45b53c55f68490
Ugotapi <rudischmitz@gmail.com>
2023-12-20 20:36:58 -0600

    Update 3up-portrait.sh

--- 3up-portrait.sh
+++ 3up-portrait.sh
@@ -18,15 +18,15 @@ cat > /home/$USER/myscript.sh << EOLP
 #!/bin/sh
 # what this script does: start chromium
 
-chromium-browser --new-window --window-position=0,0 --window-size=1080,2160 --incognito --user-data-dir=/home/$USER/.config/chromium1 \
+chromium-browser --new-window --window-position=0,0 --window-size=2160,1080 --incognito --user-data-dir=/home/$USER/.config/chromium1 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://www.google.com/finance &
 
-chromium-browser --new-window --window-position=0,1281 --window-size=1080,2160 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
+chromium-browser --new-window --window-position=0,1281 --window-size=2160,1080 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://www.cnbc.com/us-markets/ &
 
-chromium-browser --new-window --window-position=0,2562 --window-size=1080,2160 --incognito --user-data-dir=/home/$USER/.config/chromium3 \
+chromium-browser --new-window --window-position=0,2562 --window-size=2160,1080 --incognito --user-data-dir=/home/$USER/.config/chromium3 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://www.wsj.com/market-data?mod=finance_mdw_mdc &
 
