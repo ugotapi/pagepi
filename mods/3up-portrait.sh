25f7fe38e592879e47e50d7ea7cb0515ad7867bc
Ugotapi <rudischmitz@gmail.com>
2023-12-22 11:15:29 -0600

    Update 3up-portrait.sh

--- mods/3up-portrait.sh
+++ mods/3up-portrait.sh
@@ -22,11 +22,11 @@ chromium-browser --new-window --window-position=0,0 --window-size=2160,1280 --in
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://www.google.com/finance &
 
-chromium-browser --new-window --window-position=0,2562 --window-size=2160,1280 --incognito --user-data-dir=/home/$USER/.config/chromium3 \
+chromium-browser --new-window --window-position=0,1281 --window-size=2160,1280 --incognito --user-data-dir=/home/$USER/.config/chromium3 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://finviz.com/map.ashx?t=geo &
 
-chromium-browser --new-window --window-position=0,1281 --window-size=2160,1280 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
+chromium-browser --new-window --window-position=0,2562 --window-size=2160,1280 --incognito --user-data-dir=/home/$USER/.config/chromium2 \
 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter \
 --app=https://tradingview.com/markets/ &
 
